0ef4720194c5af787a5b4530285b477e0c4ee422
miko1203744 <mikwossen@gmail.com>
2022-07-01 01:11:20 -0700

    task

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,6 +1,5 @@
 #include <unistd.h>
 /**
- *
  * main - Entry point
  *     
  * Return: Always 1 (Success)
@@ -8,6 +7,6 @@
 int main(void)
 {
 char str1[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-write(2,str1,59);
+write(2 ,str1 ,59);
 return (1);
 }
